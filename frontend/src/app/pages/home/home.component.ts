f1e9e615c18ba4f638bf332481905848c8e50791
Sergio <sergiohrodriguess@gmail.com>
2025-05-21 23:28:21 -0300

    ao mudar mes, atualizando horarios corretamente

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -21,10 +21,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
     dayWithTimes: DayWithTimes[] = [];
+    dayWithTimesFiltrados: DayWithTimes[] = [];
     professionals: ResponseComDados<Professional[]> | undefined;
     date: Date | undefined;
-    month: number | undefined;
-    year: number | undefined;
+    month: number = 0;
+    year: number = 2025;
 
     constructor(private professionalService: ProfessionalService, private dayWithTimesService: DayWithTimesService) {}
 
@@ -34,16 +35,28 @@ export class HomeComponent implements OnInit {
         this.month = new Date().getMonth()+1;
         this.year = new Date().getFullYear();
 
-        const teste = [this.converterParaDataCompleta(1, this.month, this.year)];
+        const teste = [this.converterParaDataCompleta(1, this.month)];
         this.date = teste[0];
     }
 
-    converterParaDataCompleta(dia: number, mes: number, ano: number): Date {
-        return new Date(ano, mes - 1, dia);
+    converterParaDataCompleta(dia: number, mes: number): Date {
+        return new Date(this.year, mes - 1, dia);
+    }
+
+    mudarMes() {
+        if (this.date) {
+            const mesId = this.date.getMonth() + 1;
+            this.exibirHorarios(mesId);
+          }
+    }
+
+    exibirHorarios(month: number){
+        this.dayWithTimesFiltrados = this.dayWithTimes.filter(item => item.month === month);
     }
 
     async buscarDiasDisponiveis() {        
         const response = await firstValueFrom(this.dayWithTimesService.getDayWithTimes(1));
-        this.dayWithTimes = response.filter(item => item.month === this.month);
+        this.dayWithTimes = response;
+        this.exibirHorarios(this.month);
     }
 }
